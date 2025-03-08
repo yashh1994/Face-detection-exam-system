26512ebfff9ea0894512e18cb8203edcd7717a70
YASH FADADU <fadaduyash1994@gmail.com>
2025-03-08 19:02:23 +0530

    Deploy setup

--- Backend/routes.py
+++ Backend/routes.py
@@ -395,4 +395,5 @@ def test_route():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.getenv("PORT", 5000)) 
+    app.run(host='0.0.0.0', port=port, debug=True)
